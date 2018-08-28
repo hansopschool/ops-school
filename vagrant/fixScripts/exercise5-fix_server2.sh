44eed058b486ff658bec186037fb15212e5abd48
hansopschool <42460383+hansopschool@users.noreply.github.com>
2018-08-28 15:28:15 +0300

    Update exercise5-fix_server2.sh

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -9,7 +9,14 @@
 #sleep 5
 #sudo ssh-copy-id -i /home/vagrant/.ssh/id_rsa vagrant@server1
 #sed -i -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-eval `ssh-agent`
-sudo ssh-keygen
-sleep 5
-sudo ssh-copy-id -i /vagrant/id_rsa.pub vagrant@192.168.100.11
+#eval `ssh-agent`
+#sudo ssh-keygen
+#sleep 5
+#sudo ssh-copy-id -i /vagrant/id_rsa.pub vagrant@192.168.100.10
+#option5
+sed -i -e 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+sed -i -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sed -i -e 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+echo "PermitRootLogin without-password" >> /etc/ssh/sshd_config
+echo "AllowUsers root otheruser" >> /etc/ssh/sshd_config
+sudo service sshd restart
