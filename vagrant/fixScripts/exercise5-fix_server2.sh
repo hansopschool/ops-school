892c7b77d9a139d2bb83a2cea3e2892ed795c87a
hansopschool <42460383+hansopschool@users.noreply.github.com>
2018-08-28 13:55:45 +0300

    Update exercise5-fix_server2.sh

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -4,7 +4,8 @@
 #sleep 15
 #ssh-copy-id -i /home/vagrant/exit.pub vagrant@192.168.100.10
 #sudo ssh-keygen -t rsa -f /vagrant/id_rsa -q -N ''
-sudo cp /vagrant/id_rsa /home/vagrant/.ssh/id_rsa
-sudo chmod 0600 /home/vagrant/.ssh/id_rsa
-sleep 5
-sudo ssh-copy-id -i /home/vagrant/.ssh/id_rsa vagrant@server1
+#sudo cp /vagrant/id_rsa /home/vagrant/.ssh/id_rsa
+#sudo chmod 0600 /home/vagrant/.ssh/id_rsa
+#sleep 5
+#sudo ssh-copy-id -i /home/vagrant/.ssh/id_rsa vagrant@server1
+sed -i -e 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
