4aa98753034fa91e1b68b74c6d8d57d16e45e5d6
hansopschool <42460383+hansopschool@users.noreply.github.com>
2018-08-29 10:35:33 +0300

    Update exercise1-fix.sh

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #add fix to exercise1 here
 #echo 'function curl () { /usr/bin/curl -L $1 $2 $3 $4 $5 }' >> /home/vagrant/.bashrc
-#probably wget utility is missing, re-installing curl...
-sudo apt-get install curl
