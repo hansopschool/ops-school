52efdde90fe10f866b04d70eed16717c80e6d035
hansopschool <42460383+hansopschool@users.noreply.github.com>
2018-08-29 11:22:13 +0300

    Update exercise1-fix.sh

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #add fix to exercise1 here
 #echo 'function curl () { /usr/bin/curl -L $1 $2 $3 $4 $5 }' >> /home/vagrant/.bashrc
-alias curl="curl -L"
+echo 'alias curl="curl -L"' >> /home/vagrant/.bashrc
